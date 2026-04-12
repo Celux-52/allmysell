6a7e7fe08a040aa01601c1d0c02b002382e87f73
Melih Bıçak <melih20052005gs@gmail.com>
2026-04-12 20:26:40 +0300

    Remove the tested 5 items to test again

--- app/shop/ebay/page.tsx
+++ app/shop/ebay/page.tsx
@@ -10,42 +10,6 @@ export const metadata: Metadata = {
 };
 
 const products = [
-  {
-    name: 'Magnetic Phone Holder For Car, Dashboard Car Phone Holder Mount Magnetic Stainless Steel Car Phone Holder - Dashboard Mount, Water-resistant, Rotatable ',
-    description: 'Material Others Product Attributes Ordinary Package Size 56*40*40(mm) Our normal...',
-    category: 'Yeni Eklenenler',
-    image: 'https://cf.cjdropshipping.com/c8414421-384f-4c91-b47f-29f3f4f363f5.jpg',
-    link: 'https://www.ebay.com/itm/206206402394',
-    soldOut: false,
-    store: 'ymglobal',
-  },
-  {
-    name: 'Professional Camera Tripod Stand Holder Mount For Cell Phone, Portable Tripod, Mobile Phone Live Stream Holder, Camera Tripod ',
-    description: 'Material Others Product Attributes Ordinary Package Size 34*45*345(mm) Our norma...',
-    category: 'Yeni Eklenenler',
-    image: 'https://cf.cjdropshipping.com/a57d6e8d-b93d-4b70-9fc5-f745fd1809a6.jpg',
-    link: 'https://www.ebay.com/itm/206206401900',
-    soldOut: false,
-    store: 'ymglobal',
-  },
-  {
-    name: '360 Auto Face Tracking Gimbal AI Smart Gimbal Face Tracking Auto Phone Holder For Smartphone Video Vlog Live Stabilizer Tripod',
-    description: 'Description Material Plastic Product Attributes Battery Contains Package Size 18...',
-    category: 'Yeni Eklenenler',
-    image: 'https://oss-cf.cjdropshipping.com/product/2024/03/18/08/436215b7-79b6-4f55-8a6f-7eb9d470f45d.jpg',
-    link: 'https://www.ebay.com/itm/206206401465',
-    soldOut: false,
-    store: 'ymglobal',
-  },
-  {
-    name: 'Car Detail Brush Soft Brush Interior Cleaning',
-    description: 'Description Material Plastic, Others Product Attributes Ordinary Package Size 24...',
-    category: 'Yeni Eklenenler',
-    image: 'https://oss-cf.cjdropshipping.com/product/2024/06/02/01/c680b307-6d4f-4fcf-94d5-2d292a7868d6.jpg',
-    link: 'https://www.ebay.com/itm/206206401137',
-    soldOut: false,
-    store: 'ymglobal',
-  },
   // NEW TRIANGLES-3 PRODUCTS
   {
     name: 'Universal Car HUD Head Up Display Digital GPS Speedometer',
