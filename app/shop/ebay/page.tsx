502fd2a0ae77cf641dd34ebe679930dda4976aee
Melih Bıçak <melih20052005gs@gmail.com>
2026-04-14 17:19:46 +0300

    Bot: Cartoon Plush Doll P

--- app/shop/ebay/page.tsx
+++ app/shop/ebay/page.tsx
@@ -11,6 +11,15 @@ export const metadata: Metadata = {
 
 const products = [
   {
+    name: 'Cartoon Plush Doll Phone Lanyard Anti-loss',
+    description: 'Product information: Type: Phone Lanyard Color: Multicolor Specifications: [Upgr...',
+    category: 'Yeni Eklenenler',
+    image: 'https://oss-cf.cjdropshipping.com/product/2026/04/14/01/b20e2a17-9c57-4ec0-aefa-eb0fa95171db_trans.jpeg',
+    link: 'https://www.ebay.com/itm/206210210576',
+    soldOut: false,
+    store: 'ymglobal',
+  },
+  {
     name: 'Dopamine Flower Woven Phone Lanyard',
     description: 'Material Cloth Product Attributes Ordinary Package Size 120*10*50(mm) Product in...',
     category: 'Yeni Eklenenler',
