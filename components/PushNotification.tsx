0f2607a36de736037ec6887e13c0bc69ae96f1b9
Melih <info@allmysell.com>
2026-06-25 20:57:20 +0300

    fix: Remove unsupported colors property from OneSignal notifyButton config

--- components/PushNotification.tsx
+++ components/PushNotification.tsx
@@ -16,19 +16,7 @@ export default function PushNotification() {
           appId: "0e773e33-6a8a-47d7-8b56-4c02945ff065",
           allowLocalhostAsSecureOrigin: true, // Localhost'ta test edebilmek için
           notifyButton: {
-            enable: true, // Sağ altta küçük bir zil ikonu çıkarır (isteğe bağlı kapatılabilir)
-            colors: {
-              'circle.background': 'rgb(79, 70, 229)', // Indigo 600
-              'circle.foreground': 'white',
-              'badge.background': 'rgb(79, 70, 229)',
-              'badge.foreground': 'white',
-              'badge.bordercolor': 'white',
-              'pulse.color': 'white',
-              'dialog.button.background.hovering': 'rgb(67, 56, 202)', // Indigo 700
-              'dialog.button.background.active': 'rgb(67, 56, 202)',
-              'dialog.button.background': 'rgb(79, 70, 229)',
-              'dialog.button.foreground': 'white',
-            }
+            enable: true,
           },
         });
         
